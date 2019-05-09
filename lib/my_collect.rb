82a100229bc0827eabe9e29297f14c768ed6c09d
moniet <moniet.sawhney@gmail.com>
2019-05-09 10:50:05 +0100

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(arr) 
+  # loop through the array 
+  # apply the condition to the array 
+  # push to new array and return new array
+  new_arr = []
+  i = 0
+  while i < arr.length 
+    new_arr << yield(arr[i])
+    i +=1 
+  end  
+  new_arr
+end 
+
 
 
